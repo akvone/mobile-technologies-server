ee7f368ed594ae5052d7cabd19805c44c52ff604
Kirill Vialkov <akvone@mail.ru>
2019-11-21 18:47:10 +0300

    Get rid of duplicate mappings

--- src/main/kotlin/com/akvone/mobiletechnologies/controllers/MeasurementController.kt
+++ src/main/kotlin/com/akvone/mobiletechnologies/controllers/MeasurementController.kt
@@ -6,17 +6,17 @@ import com.akvone.mobiletechnologies.services.MainService
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/v1")
-class MainController(val mainService: MainService) {
+@RequestMapping("/api/v1/measurements")
+class MeasurementController(val mainService: MainService) {
 
-    @PostMapping("/measurement")
+    @PostMapping
     fun createMeasurement(@RequestBody newMeasurement: MeasurementRequestDTO) {
         mainService.createMeasurement(newMeasurement)
     }
 
-    @GetMapping("/measurements")
+    @GetMapping
     fun getMeasurements(): List<Measurement> = mainService.getMeasurements()
 
-    @GetMapping("/measurement")
+    @GetMapping("/{id}")
     fun getMeasurement(@PathVariable id: String): Measurement = mainService.getMeasurement(id)
 }
