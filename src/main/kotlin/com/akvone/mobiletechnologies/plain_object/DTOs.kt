49d10c9639154132731b168af74ac4018b61acb0
Kirill Vialkov <akvone@mail.ru>
2020-01-11 19:41:48 +0300

    Refactor DTOs

--- src/main/kotlin/com/akvone/mobiletechnologies/plain_object/DTOs.kt
+++ src/main/kotlin/com/akvone/mobiletechnologies/plain_object/DTOs.kt
@@ -2,10 +2,20 @@ package com.akvone.mobiletechnologies.plain_object
 
 import java.time.LocalDateTime
 
-data class MeasurementRequestDTO(val bpm: Int, val lower: Int, val upper: Int)
-data class MeasurementDTO(val id: String, val createdAt: LocalDateTime,
-                          val bpm: Int, val lower: Int, val upper: Int)
+data class MeasurementRequestDTO(val bpm: Int,
+                                 val lower: Int,
+                                 val upper: Int)
 
-data class UserRequestDTO(var username: String, val password: String)
+data class MeasurementDTO(val id: String,
+                          val createdAt: LocalDateTime,
+                          val bpm: Int,
+                          val lower: Int,
+                          val upper: Int)
 
-data class ProfileDTO(val name: String, val age: Int?, val height: Int?, val weight: Double?)
+data class UserRequestDTO(val username: String,
+                          val password: String)
+
+data class ProfileDTO(val name: String,
+                      val age: Int?,
+                      val height: Int?,
+                      val weight: Double?)
